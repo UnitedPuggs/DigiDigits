26b2f9f3464c43c1476684f105296dc01760b87f
UnitedPuggs <edpoulsonv@hotmail.com>
2023-05-14 23:38:19 -0700

    fix: performance from 5 mins -> 7s, more pythonic variable names

--- digidigits.py
+++ digidigits.py
@@ -1,55 +1,68 @@
 import requests
 import pandas as pd
 import pygsheets
+import aiohttp
+import asyncio
+import time
+
 
 #uses service account key to authorize sheet api
 gc = pygsheets.authorize(service_file='digikey.json')
 #request to get data for all digimon cards and convert to json
-allcards = requests.get("https://digimoncard.io/api-public/getAllCards.php?sort=name&series=Digimon%20Card%20Game&sortdirection=asc%27")
-alldict = allcards.json()
+allcards = requests.get("https://digimoncard.io/api-public/getAllCards.php?sort=name&series=Digimon%20Card%20Game&sortdirection=asc%27").json()
 
-#data is info on every card queried from alldicts
-data = []
-lenBetweenSets = []
 #holds the length of every request since lengths are uneven
-session = requests.Session()
+card_amt = []
 
-for i in range(len(alldict)):
-    querystring = alldict[i]["cardnumber"]
-    response = session.get(f"https://digimoncard.io/api/tcgplayerprices.php?cardnumber={querystring}")
-    data += response.json()
-    if "error" in data: #sometimes cards don't exist, so filters out errored out dicts
-        data.remove("error")
-    lenBetweenSets.append(len(data))
+card_name = []
+card_num = []
+card_pack = []
+price_list = []
 
+start = time.time()
+
+async def get_card(session, url):
+    async with session.get(url) as resp:
+        digimon = await resp.json()
+        return digimon
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for i in range(len(allcards)):
+            query = allcards[i]["cardnumber"]
+            url = f'https://digimoncard.io/api/tcgplayerprices.php?cardnumber={query}'
+            tasks.append(asyncio.ensure_future(get_card(session, url)))
+        
+        gathered_cards = await asyncio.gather(*tasks)
+        for cards in gathered_cards:
+            if not 'error' in cards:
+                card_amt.append(len(cards))
+                for i in range(len(cards)):
+                    price_list.append(cards[i]['market_price'])
+                    card_pack.append(cards[i]['name'])
+            else: #I hate this API
+                card_amt.append(0)
+
+asyncio.run(main())
 #Creates an empty dataframe
 nameFrame = pd.DataFrame()
 numFrame = pd.DataFrame()
 priceFrame = pd.DataFrame()
-
-#Lists for each column that will exist
-cardNames = []
-cardNums = []
-priceList = []
-
-for i in range(len(data)):
-    priceList.append(data[i]["market_price"])
+pack_frame = pd.DataFrame()
 
 #For amount of cards in each set of data with attached names and card numbers
-for i in range(len(lenBetweenSets)):
-    if i == 0:
-        for j in range(0, lenBetweenSets[0]):
-            cardNames.append(alldict[i]["name"])
-            cardNums.append(alldict[i]["cardnumber"])
-    else:
-        for k in range(0, (lenBetweenSets[i] - lenBetweenSets[i - 1])):
-            cardNames.append(alldict[i]["name"])
-            cardNums.append(alldict[i]["cardnumber"])
+for i in range(len(card_amt)):
+    for j in range(0, card_amt[i]):
+        card_name.append(allcards[i]['name'])
+        card_num.append(allcards[i]['cardnumber'])
+
 
 #Creates the columns
-nameFrame['Card Names'] = cardNames
-numFrame['Card Numbers'] = cardNums
-priceFrame['Market Price'] = priceList
+nameFrame['Card Names'] = card_name
+numFrame['Card Numbers'] = card_num
+priceFrame['Market Price'] = price_list
+pack_frame['Pack'] = card_pack
     
 #Opens Google Sheet
 sh = gc.open('DigiDigits')
@@ -60,3 +73,7 @@ wks = sh[0]
 wks.set_dataframe(nameFrame,(1,1))
 wks.set_dataframe(numFrame,(1,2))
 wks.set_dataframe(priceFrame,(1,3))
+wks.set_dataframe(pack_frame, (1, 4))
+
+end = time.time() - start
+print(f'{end} seconds')
