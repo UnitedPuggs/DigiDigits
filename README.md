7bd721db2d4f02b640a042178a9ad6f8801d4ae4
UnitedPuggs <edpoulsonv@hotmail.com>
2023-05-17 20:32:10 -0700

    might be a useless commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DigiDigits
-Python script that utilizes API calls for monetary comparisons as market prices for each individual cards shift. The end goal is to be able to find the greater positive and negative changes in market price.
+Python script that utilizes a very sneaky little API to get market data for all Digimon cards. 
+## Features to come
+- Currently working on storing market data every week to create interactive graphs that visualize market price changes for these cards. 
+- May attempt to do some time-series forecasting and regression analysis to predict future prices for cards! :)
+
--- digidigits.py
+++ digidigits.py
@@ -14,10 +14,12 @@ allcards = requests.get("https://digimoncard.io/api-public/getAllCards.php?sort=
 #holds the length of every request since lengths are uneven
 card_amt = []
 
+#Lists for name, number, pack, and market prices per card
 card_name = []
 card_num = []
 card_pack = []
 price_list = []
+rarities = []
 
 start = time.time()
 
@@ -26,7 +28,7 @@ async def get_card(session, url):
         digimon = await resp.json()
         return digimon
 
-async def main():
+async def main(): 
     async with aiohttp.ClientSession() as session:
         tasks = []
         for i in range(len(allcards)):
@@ -41,6 +43,7 @@ async def main():
                 for i in range(len(cards)):
                     price_list.append(cards[i]['market_price'])
                     card_pack.append(cards[i]['name'])
+                    rarities.append(cards[i]['rarity'])
             else: #I hate this API
                 card_amt.append(0)
 
@@ -50,15 +53,15 @@ for i in range(len(card_amt)):
     for j in range(0, card_amt[i]):
         card_name.append(allcards[i]['name'])
         card_num.append(allcards[i]['cardnumber'])
+
 #Creates our DataFrame and populates it with values from our lists
-digi_frame = pd.DataFrame({'Card Names': card_name, 'Card Numbers': card_num, 'Market Price': price_list, 'Pack': card_pack})
-print(digi_frame['Market Price'])
+digi_frame = pd.DataFrame({'Card Names': card_name, 'Card Numbers': card_num, 'Market Price': price_list, 'Pack': card_pack, 'Rarity': rarities})
 #Opens Google Sheet
 sh = gc.open('DigiDigits')
 #Uses the first sheet
 wks = sh[0]
 
-#Sets data frame to cell starting at 1, 1 to 1, 3
+#Sets data frame to cell starting at 1, 1 
 wks.set_dataframe(digi_frame, (1,1))
 
 end = time.time() - start
